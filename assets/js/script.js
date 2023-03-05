74ebc11ff59370b228e5866b44f25cd3b0cf8bf7
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-05 17:03:02 -0300

    Parametros passados entre páginas com JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-
-function ativarLink(link){
-   const href = link.href;
-   const url = window.location.href;
-     
-   if(url.includes(href)){
-      console.log(link);
-      link.classList.add('ativo')
-   }
-
+// Ativação do link do menu - Pagina atual
+function ativarLink(link) {
+  const href = link.href;
+  const url = window.location.href;
+  if (url.includes(href)) {
+    console.log(link);
+    link.classList.add("ativo");
+  }
 }
-const links = document.querySelectorAll('.header-menu a').forEach(ativarLink);
+const links = document.querySelectorAll(".header-menu a").forEach(ativarLink);
+
+// Ativar itens do orçamento
+const url = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+url.forEach(ativarProduto);
