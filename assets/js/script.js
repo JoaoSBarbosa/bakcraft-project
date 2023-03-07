4c02d1a17e3984838322d69a5eb77b26315c6896
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-06 21:40:29 -0300

    Logica JS para galeria de imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,18 +19,34 @@ function ativarProduto(parametro) {
 url.forEach(ativarProduto);
 
 // Perguntas frequentes
-function ativarPergunta(event){
+function ativarPergunta(event) {
   const p = event.currentTarget;
-  const controls = p.getAttribute('aria-controls');
+  const controls = p.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-  resposta.classList.toggle('ativo');
-  const condicao = resposta.classList.contains('ativo');
-  p.setAttribute('aria-expanded',condicao)
+  resposta.classList.toggle("ativo");
+  const condicao = resposta.classList.contains("ativo");
+  p.setAttribute("aria-expanded", condicao);
 }
 
-function eventosPerguntas(pergunta){
-  pergunta.addEventListener('click', ativarPergunta)
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
-const perguntas = document.querySelectorAll('.perguntas button').forEach(eventosPerguntas);
+const perguntas = document
+  .querySelectorAll(".perguntas button")
+  .forEach(eventosPerguntas);
 
+// Galeria de Bicicletas
+function trocarImagem(event) {
+  const imgAtual = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(imgAtual);
+  }
+}
+const galeria = document
+  .querySelectorAll(".bicicleta-imagens img")
+  .forEach((img) => {
+    img.addEventListener("click", trocarImagem);
+  });
 
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
